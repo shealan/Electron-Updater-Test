fe34f973034aae1c6ac1fbd62a02807aa7d61d47
Shealan Forshaw <shealan@mentallyfriendly.com>
2018-06-26 11:57:58 +1000

    Added github build info

--- electron.js
+++ electron.js
@@ -1,22 +1,28 @@
-const {app, BrowserWindow, ipcMain} = require('electron');
-const {autoUpdater} = require("electron-updater");
-let win; // this wills store the window object
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { autoUpdater } = require("electron-updater");
+
+autoUpdater.logger = require("electron-log")
+autoUpdater.logger.transports.file.level = "info"
+
+let win;
 
 function createDefaultWindow() {
     win = new BrowserWindow({width: 900, height: 680});
     win.loadURL(`file://${__dirname}/index.html`);
     win.on('closed', () => app.quit());
-  return win;
+    return win;
 }
 
 // when the update is ready, notify the BrowserWindow
 autoUpdater.on('update-downloaded', (info) => {
     win.webContents.send('updateReady')
 });
+
 app.on('ready', function() {
   createDefaultWindow();
   autoUpdater.checkForUpdates();
 });
+
 ipcMain.on("quitAndInstall", (event, arg) => {
     autoUpdater.quitAndInstall();
 })
